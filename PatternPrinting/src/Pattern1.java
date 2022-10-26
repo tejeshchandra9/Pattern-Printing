bc120ea799016c1a48fd810d14ee15edd4612bfd
Tejesh <Tejesh@LAPTOP-QU5JLBRA>
2022-10-25 22:42:00 -0500

    Assignment-1

---
+++ PatternPrinting/src/Pattern1.java
@@ -0,0 +1,99 @@
+
+public class Pattern1 {
+	public static void main(String[] args) {
+		int n=22;
+	    for(int i=0;i<n;i++)
+	    {
+	    	for(int j=0;j<n;j++)
+	    	{
+	    		if(i==n/4&&j>=n/4&&j<=(3*n)/4&&i<=(3*n)/4 || j==(n-1)/2&&i>=n/4&&j<=(3*n)/4&&i<=(3*n)/4 || i==(3*n)/4&&j>=n/4&&j<=(3*n)/4)
+	    		{
+	    			System.out.print("*");
+	    		}
+	    		else {
+	    			System.out.print(" ");
+	    		}
+	    	}
+	    	
+	    	for(int j=0;j<n;j++)
+	    	{
+	    		if(j==n/4&&i>=n/4&&i<=(3*n)/4 ||j>=n/4&&j==i&&j<=(3*n)/4||j==(3*n)/4&&i>=n/4&&i<=(3*n)/4) 
+	    		{
+	    		    System.out.print("*");
+	    		}
+	    		    
+	    		else
+	    		{
+	    	         System.out.print(" ");
+	    		}
+	    	}
+	    	for(int j=0;j<n;j++)
+	    	{
+	    		if(j==n/4&&i>=n/4&&i<=(3*n)/4||i==n/4&&j>n/4&&j<=(3*n)/4||i==(n-1)/2&&j>n/4&&j<=(3*n)/4||i==(3*n)/4&&j>n/4&&j<=(3*n)/4) 
+	    		{
+	    		    System.out.print("*");
+	    		}
+	    		    
+	    		else
+	    		{
+	    	         System.out.print(" ");
+	    		}
+	    	}
+	    	for(int j=0;j<n;j++)
+	    	{
+	    		if(j==n/4&&i>=n/4&&i<(3*n)/4 || i==(3*n)/4&&j>n/4&&j<(3*n)/4 || j==(3*n)/4&&i>=n/4&&i<(3*n)/4 )
+	    		{
+	    		    System.out.print("*");
+	    		}
+	    		    
+	    		else
+	    		{
+	    	         System.out.print(" ");
+	    		}
+	    	}
+	    	for(int j=0;j<n;j++)
+	    	{
+	    		if(j==n/4&&i>=n/4&&i<=(3*n)/4||i==n/4&&j>n/4&&j<(3*n)/4||j==(3*n)/4&&i>n/4&&i<(n-1)/2||i==(n-1)/2&&j>n/4&&j<(3*n)/4||j==i&&j>(n-1)/2&&i<=(3*n)/4 )
+	    		{
+	    		    System.out.print("*");
+	    		}
+	    		    
+	    		else
+	    		{
+	    	         System.out.print(" ");
+	    		}
+	    	}
+	    	for(int j=0;j<n;j++)
+	    	{
+	    		if(i==n/4&&j>n/4&&j<(3*n)/4||j==n/4&&i>n/4&&i<(3*n)/4 || i==(3*n)/4&&j>n/4&&j<(3*n)/4 || j==(3*n)/4&&i>n/4&&i<(3*n)/4)
+	    		{
+	    		    System.out.print("*");
+	    		}
+	    		    
+	    		else
+	    		{
+	    	         System.out.print(" ");
+	    		}
+	    	}
+	    	for(int j=0;j<n;j++)
+	    	{
+	    		if(j==n/4&&i>=n/4&&i<=(3*n)/4 ||j>=n/4&&j==i&&j<=(3*n)/4||j==(3*n)/4&&i>=n/4&&i<=(3*n)/4) 
+	    		{
+	    		    System.out.print("*");
+	    		}
+	    		    
+	    		else
+	    		{
+	    	         System.out.print(" ");
+	    		}
+	    	}
+	    	
+	    	System.out.println("  ");
+	    }
+
+	} 
+
+}
+
+
+
